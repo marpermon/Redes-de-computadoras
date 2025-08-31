e51f030c9df93226a39fb2df21f2f54d099f6063
marpermon <maria.perezmonge@ucr.ac.cr>
2025-08-31 13:30:24 -0600

    retornando bits en formato string

--- Tarea1/clases.py
+++ Tarea1/clases.py
@@ -1,8 +1,14 @@
+# Cantidad de bits
+MAC = 8
+IP = 8
+Puerto = 16
+AppCodigo = 2
+
 class PC:
     APP_CODE = {
-        "Telegram": "9",
-        "WhatsApp": "10",
-        "Facebook": "11",
+        "Telegram": 0,
+        "WhatsApp": 1,
+        "Facebook": 2,
     }
     APP_PORT = {
         "Telegram": 443,
@@ -44,39 +50,29 @@ class PC:
         return payload_t4
 
     def capa_red(self, dst_pc, payload_t4):
-        # Para el texto mostramos últimos octetos (simplificación)
         payload_t3 = f"{self.ip},{dst_pc.ip},{payload_t4}"
         print(f"Capa 3 (Red): {payload_t3}")
         return payload_t3
 
     def capa_enlace(self, dst_pc, payload_t3):
-        # Para el texto usamos MACs en enteros cortos (src dst ...)
         payload_t2 = f"{self._fmt_mac(self.mac)},{self._fmt_mac(dst_pc.mac)},{payload_t3}"
         print(f"Capa 2 (Enlace de Datos): {payload_t2}")
         return payload_t2
 
-    def capa_fisica(self, payload_t4):
-        """
-        Serializa a bits:
-        - MAC src: 8 bits
-        - MAC dst: 8 bits
-        - IP src: 8 bits
-        - IP dst: 8 bits
-        - Puerto: 16 bits
-        - Payload (app_code + ' ' + mensaje): ASCII
-        """
-        self_mac, dst_pc_mac, self_ip, dst_pc_ip, port, app_code, mensaje = payload_t4.split(',')
-        bits = []
-        bits.append(self._to_bits(self_mac, 8))           # MAC src 8b
-        bits.append(self._to_bits(dst_pc_mac, 8))        # MAC dst 8b
-        bits.append(self._to_bits(self_ip, 8))       # IP src 8b
-        bits.append(self._to_bits(dst_pc_ip, 8))     # IP dst 8b
-        bits.append(self._to_bits(port, 16))              # Puerto 16b
-        payload_ascii = f"{app_code} {mensaje}"
-        bits.append(self._ascii_bits(payload_ascii))      # ASCII
-        bitstream = ''.join(bits)
-        print(f"Capa 1 (Física): {bitstream}")
-        return bitstream
+    def capa_fisica(self, payload_t4, enviar):
+        if enviar:
+            self_mac, dst_pc_mac, self_ip, dst_pc_ip, port, app_code, mensaje = payload_t4.split(',')
+            bits = ""
+            bits = bits + self._to_bits(self_mac, MAC)     # MAC src 8b
+            bits = bits + self._to_bits(dst_pc_mac, MAC)   # MAC dst 8b
+            bits = bits + self._to_bits(self_ip, IP)       # IP src 8b
+            bits = bits + self._to_bits(dst_pc_ip, IP)     # IP dst 8b
+            bits = bits + self._to_bits(port, Puerto)      # Puerto 16b
+            bits = bits + self._to_bits(app_code, AppCodigo)   # AppCodigo 16b
+            payload_ascii = f"{mensaje}"
+            bits = bits + self._ascii_bits(payload_ascii)      # ASCII
+            print(f"Capa 1 (Física): {bits}")
+            return bits
 
     @staticmethod
     def _fmt_mac(mac_int):
@@ -84,7 +80,7 @@ class PC:
         return f"{mac_int:02d}"
 
     # --- flujo completo ---
-    def enviar(self, dst_pc, app_name, mensaje, imprimir=True):
+    def enviar(self, dst_pc, app_name, mensaje):
         print("Iniciando encapsulamiento.")
         # Capa 5
         payload_app = self.capa_aplicacion(app_name, mensaje)
@@ -97,5 +93,5 @@ class PC:
         # Capa 1
         bitstream = self.capa_fisica(dst_pc, mensaje)
 
-        # Regresa numero binario
+        # Regresa numero binario formato string
         return bitstream
